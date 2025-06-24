96888602d052cdf1e5b7fdfaa5475683ca678c1a
Tom Laskowski <44295192+fotomash@users.noreply.github.com>
2025-06-24 22:25:52 +0100

    Ensure logging file path directories exist

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,8 @@
 import os
 
+# Ensure lightweight test mode is enabled before the package is imported
+os.environ.setdefault("ZANALYTICS_TEST_MODE", "1")
+
 def pytest_configure():
-    os.environ.setdefault("ZANALYTICS_TEST_MODE", "1")
+    """Additional pytest configuration can be added here."""
+    pass
