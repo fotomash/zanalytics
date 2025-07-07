d42fadb4b40c19ff33e2cbbddbb9535c8808e126
Tom Laskowski <44295192+fotomash@users.noreply.github.com>
2025-07-07 19:12:30 +0100

    Update run_converter.sh

--- dashboard/run_converter.sh
+++ dashboard/run_converter.sh
@@ -1,8 +1,12 @@
 
+
+cp -R /Volumes/utils/out/parquet* /Users/tom/Documents/_trade/_exports/_tick/parquet
+
+'''
+cp -R /Volumes/GitHub/zanalytics/utils/out/parquet/* /Users/tom/Documents/_trade/_exports/_tick/parquet
+cp -R /Volumes/_tick_csv/* /Users/tom/Documents/_trade/_exports/_tick/_raw
 cp -R /Volumes/_bars/* /Users/tom/Documents/_trade/_exports/_tick/_bars
 cp -R /Volumes/_processed/* /Users/tom/Documents/_trade/_exports/_tick/midas_analysis
 cp -R /Volumes/_ticks/* /Users/tom/Documents/_trade/_exports/_tick/_raw
-cp -R /Volumes/out/* /Users/tom/Documents/_trade/_exports/_tick/parquet
-cp -R /Volumes/GitHub/zanalytics/utils/out/parquet/* /Users/tom/Documents/_trade/_exports/_tick/parquet
-cp -R /Volumes/_tick_csv/* /Users/tom/Documents/_trade/_exports/_tick/_raw
+'''
 
